036ceff4df22d2e73f874b6902d57742bc5395f0
Timm4J <109731957+Timm4J@users.noreply.github.com>
2024-06-04 08:10:56 +0200

    Update __init__.py
    asdasddadsda
    oi bruv, it's chewsday, innit?

--- Project One/ticket/__init__.py
+++ Project One/ticket/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = ""
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# app.config['SECRET_KEY'] = 'f1c50cdf58a5ac7024799454'
+app.config['SECRET_KEY'] = 'f1c50cdf58a5ac7024799454'
 
 
 db = SQLAlchemy(app)
